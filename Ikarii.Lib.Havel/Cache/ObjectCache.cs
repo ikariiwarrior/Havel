5fe1bea90a4b369221dec58d9b2d886edf74f263
Shawn Hall <shawn+gitkraken@ikarii.com>
2019-10-11 14:54:32 -0700

    Minor updates to conform to PersistCore updates

--- Ikarii.Lib.Havel/Cache/ObjectCache.cs
+++ Ikarii.Lib.Havel/Cache/ObjectCache.cs
@@ -23,6 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 *****************************************************************************/
 
+
 namespace Ikarii.Lib.Havel.Cache
 {
    using System;
@@ -36,36 +37,54 @@ namespace Ikarii.Lib.Havel.Cache
    /// <summary>
    /// 
    /// </summary>
-   public class TypeCache
+   internal class ObjectCache
    {
 	  private static CancellationTokenSource m_reset_cache_token = new CancellationTokenSource();
-	  private static readonly Lazy<TypeCache> m_instance = new Lazy<TypeCache>( () => new TypeCache() );
+	  private static readonly Lazy<ObjectCache> m_instance = new Lazy<ObjectCache>( () => new ObjectCache() );
 	  private IMemoryCache m_cache;
 
-	  public static TypeCache Instance { get { return m_instance.Value; } }
+	  internal static ObjectCache Instance { get { return m_instance.Value; } }
 
-	  private TypeCache() => this.m_cache = new MemoryCache( new MemoryCacheOptions() );
+	  private ObjectCache() => this.m_cache = new MemoryCache( new MemoryCacheOptions() );
 
-	  public static void AddType( Type type, TypeMap map ) => Instance.InternalAddType( type, map );
-	  private void InternalAddType( Type type, TypeMap map )
+	  private string GetCacheKey( Type t, object id ) => ( $"{t.FullName}::{id}" );
+
+	  internal static void AddObject( object id, object o ) => Instance.AddObjectInternal( id, o );
+	  private void AddObjectInternal( object id, object o )
 	  {
+		 var cache_key = this.GetCacheKey( o.GetType(), id );
 		 var options = new MemoryCacheEntryOptions()
 			.SetSlidingExpiration( TimeSpan.FromMinutes( 60 ) )
 			.SetAbsoluteExpiration( DateTimeOffset.UtcNow.AddDays( 1 ) );
 
 		 options.AddExpirationToken( new CancellationChangeToken( m_reset_cache_token.Token ) );
 
-		 this.m_cache.Set( type.FullName, map, options );
+		 this.m_cache.Set( cache_key, o, options );
+
 	  }
 
-	  public static TypeMap GetType( Type type ) => Instance.InternalGetType( type );
-	  private TypeMap InternalGetType( Type type ) => this.m_cache.Get<TypeMap>( type.FullName );
-
-	  public static bool ContainsType( Type type ) => Instance.InternalContainsType( type );
-	  private bool InternalContainsType( Type type )
+	  internal static bool TryGetObject<T>( object id, out T cached_object ) => Instance.InternalTryGetObject<T>( id, out cached_object );
+	  private bool InternalTryGetObject<T>( object id, out T cached_object )
 	  {
-		 var result = this.m_cache.TryGetValue<TypeMap>( type, out var cached_type);
-		 return ( result );
+		 var cache_key = this.GetCacheKey( typeof(T), id);
+		 var is_cached = this.m_cache.TryGetValue( cache_key, out cached_object );
+		 return ( is_cached );
+	  }
+
+	  internal static bool TryGetObject( object id, Type type, out object cached_object ) => Instance.InternalTryGetObject( id, type, out cached_object );
+	  private bool InternalTryGetObject( object id, Type type, out object cached_object )
+	  {
+		 var cache_key = this.GetCacheKey( type, id);
+		 var is_cached = this.m_cache.TryGetValue( cache_key, out cached_object );
+		 return ( is_cached );
+	  }
+
+	  internal static void RemoveObject<T>( object id ) => Instance.InternalRemoveObject( id, typeof( T ) );
+	  internal static void RemoveObject( object id, Type type ) => Instance.InternalRemoveObject( id, type );
+	  private void InternalRemoveObject( object id, Type type )
+	  {
+		 var cache_key = this.GetCacheKey( type, id);
+		 this.m_cache.Remove( cache_key );
 	  }
 
 	  public static void Clear() => Instance.InternalClear();
@@ -78,6 +97,5 @@ namespace Ikarii.Lib.Havel.Cache
 		 }
 		 m_reset_cache_token = new CancellationTokenSource();
 	  }
-
    }
 }
